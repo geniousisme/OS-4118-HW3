cad97abee9230f97550211ffc9487cb9c1712d88
Chen-Yu Yang <cy2417@columbia.edu>
2015-10-14 01:31:54 -0400

    The set/get intensity work\!

--- flo-kernel/kernel/light.c
+++ flo-kernel/kernel/light.c
@@ -2,16 +2,22 @@
 #include <linux/uaccess.h>
 #include <linux/light.h>
 
+struct light_intensity intensity;
+
 SYSCALL_DEFINE1(set_light_intensity, struct light_intensity __user *,
-	user_light_intensit)
+	user_light_intensity)
 {
-	printk("set_light_intensity is called!");
+	if (copy_from_user(&intensity, user_light_intensity,
+		sizeof(struct light_intensity)))
+		return -EINVAL;
 	return 0;
 }
 
 SYSCALL_DEFINE1(get_light_intensity, struct light_intensity __user *,
 	user_light_intensity)
 {
-	printk("get_light_intensity is called!");
+	if (copy_to_user(user_light_intensity, &intensity,
+		sizeof(struct light_intensity)))
+		return -EINVAL;
 	return 0;
 }
