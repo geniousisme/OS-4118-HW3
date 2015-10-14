13cfc0bae7a43d92d01506800ab5b835fa7c7cfb
Chen-Yu Yang <cy2417@columbia.edu>
2015-10-14 15:11:04 -0400

    Add comments for each system call

--- flo-kernel/kernel/light.c
+++ flo-kernel/kernel/light.c
@@ -4,6 +4,16 @@
 
 struct light_intensity intensity;
 
+/*
+ * Set current ambient intensity in the kernel.
+ *
+ * The parameter user_light_intensity is the pointer to the address
+ * where the sensor data is stored in user space. Follow system call
+ * convention to return 0 on success and the appropriate error value
+ * on failure.
+ *
+ * syscall number 378
+ */
 SYSCALL_DEFINE1(set_light_intensity, struct light_intensity __user *,
 	user_light_intensity)
 {
@@ -13,6 +23,16 @@ SYSCALL_DEFINE1(set_light_intensity, struct light_intensity __user *,
 	return 0;
 }
 
+/*
+ * Retrive the scaled intensity set in the kernel.
+ *
+ * The same convention as the previous system call but
+ * you are reading the value that was just set. 
+ * Handle error cases appropriately and return values according to convention.
+ * The calling process should provide memory in userspace to return the intensity.
+ *
+ * syscall number 379
+ */
 SYSCALL_DEFINE1(get_light_intensity, struct light_intensity __user *,
 	user_light_intensity)
 {
@@ -22,23 +42,58 @@ SYSCALL_DEFINE1(get_light_intensity, struct light_intensity __user *,
 	return 0;
 }
 
+/*
+ * Create an event based on light intensity.
+ *
+ * If frequency exceeds WINDOW, cap it at WINDOW.
+ * Return an event_id on success and the appropriate error on failure.
+ *
+ * system call number 380
+ */
 SYSCALL_DEFINE1(light_evt_create, struct event_requirements __user *,
 	intensity_params)
 {
 	return 0;
 }
 
+/*
+ * Block a process on an event.
+ *
+ * It takes the event_id as parameter. The event_id requires verification.
+ * Return 0 on success and the appropriate error on failure.
+ *
+ * system call number 381
+ */
 SYSCALL_DEFINE1(light_evt_wait, int, event_id)
 {
 	return 0;
 }
 
+/*
+ * The light_evt_signal system call.
+ *
+ * Takes sensor data from user, stores the data in the kernel,
+ * and notifies all open events whose
+ * baseline is surpassed.  All processes waiting on a given event 
+ * are unblocked.
+ *
+ * Return 0 success and the appropriate error on failure.
+ *
+ * system call number 382
+ */
 SYSCALL_DEFINE1(light_evt_signal, struct light_intensity __user *,
 	user_light_intensity)
 {
 	return 0;
 }
 
+/*
+ * Destroy an event using the event_id.
+ *
+ * Return 0 on success and the appropriate error on failure.
+ *
+ * system call number 383
+ */
 SYSCALL_DEFINE1(light_evt_destroy, int, event_id)
 {
 	return 0;
