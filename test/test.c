a37febb7874f0a1077e3f0a31a77f39f90404923
Chen-Yu Yang <cy2417@columbia.edu>
2015-10-14 21:08:54 -0400

    Add test program, not fully test yet

--- test/test.c
+++ test/test.c
@@ -1,11 +1,63 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include "light.h"
 
-int main (void)
+#define PRINT_ERRNO() (printf("error: %s\n", strerror(errno)))
+
+#define LOW 5000
+#define MEDIUM 10000
+#define HIGH 15000
+#define N 3
+#define SLEEP (60 * 1000 * 1000)
+#define WINDOW 20
+
+char *msg(int intensity)
 {
-	/* Write your test program here */
+	if (intensity == LOW)
+		return "low";
+	else if (intensity == MEDIUM)
+		return "medium";
+	else if (intensity == HIGH)
+		return "high";
+	return "unknown";
+}
+
+void wait_on_light_event(int intensity)
+{
+	pid_t pid = fork();
+
+	if (pid < 0) {
+		PRINT_ERRNO();
+		exit(EXIT_FAILURE);
+	} else if (pid == 0) {
+		int event_id;
+		struct event_requirements req = {
+			.req_intensity = intensity,
+			.frequency = WINDOW / 2
+		};
+
+		event_id = syscall(380, &req);
+		printf("process %d waits on event %d\n", getpid(), event_id);
+		syscall(381, event_id);
+		printf("%d detected a %s intensity event\n", getpid(),
+							msg(intensity));
+		exit(EXIT_SUCCESS);
+	}
+}
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < N; i++) {
+		wait_on_light_event(LOW);
+		wait_on_light_event(MEDIUM);
+		wait_on_light_event(HIGH);
+	}
+	usleep(SLEEP);
 	return 0;
 }
